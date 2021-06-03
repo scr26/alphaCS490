6d406f909f3d81d9875dc4be4cd4fe8a1d47edcf
scr26 <32083670+scr26@users.noreply.github.com>
2021-06-03 06:34:02 -1000

    Update app.py

--- app.py
+++ app.py
@@ -56,5 +56,10 @@ def logout():
     return redirect('/home')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(
+    port=int(os.getenv('PORT',8080)),
+    host=os.getenv('IP','0.0.0.0'),
+    debug=True
+    
+    
+)

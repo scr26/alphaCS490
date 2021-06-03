0a98ad36e8332f0a80e718ce14c2fa998fb5ddb3
scr26 <32083670+scr26@users.noreply.github.com>
2021-06-03 06:13:01 -1000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'cs490-summer'
+
+admin = {"username":"admin", "password":"123"}
+
+user1 = {"username":"test", "password":"test"}
+user2 = {"username":"cs490", "password":"NJIT"}
+user3 = {"username":"user", "password":"pass"}
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if(request.method == 'POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == admin['username'] and password == admin['password']:
+            session['user'] = username
+            return redirect('/adminPage')
+        
+        if username == user1['username'] and password == user1['password']:
+            session['user'] = username
+            return redirect('/landing')
+
+        if username == user2['username'] and password == user2['password']:
+            session['user'] = username
+            return redirect('/landing')
+        
+        if username == user3['username'] and password == user3['password']:
+            session['user'] = username
+            return redirect('/landing')
+
+    return render_template("login.html")
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    return render_template("register.html")
+
+@app.route('/adminPage')
+def adminPage():
+    if('user' in session and session['user'] == admin['username']):
+        return render_template("admin.html", name=admin['username'])
+
+@app.route('/landing')
+def landing():
+    return render_template("landing.html")
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect('/home')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
